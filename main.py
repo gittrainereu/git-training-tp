b7d94cd4786f57f89ba689cf30ce9be0546e6621
Tetyana Peterburgska <tetyana.peterburgska@now-gmbh.de>
2024-12-10 12:33:25 +0000

    Squashed commit of the following
    commit 8f04bd98 
    Author: tetyana.peterburgska <tetyana.peterburgska@now-gmbh.de> 
    Date: Tue Dec 10 2024 13:28:53 GMT+0100 (Central European Standard Time) 
    
        nokia def

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def samsung(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def Nokia(nokiamodell):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {nokiamodell}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
